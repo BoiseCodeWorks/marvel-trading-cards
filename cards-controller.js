f97d620bff832296ea42be48e749d7a83531a2d2
Jake Overall <joverall22@gmail.com>
2017-01-30 15:13:45 -0700

    whoops ready should only call draw

--- cards-controller.js
+++ cards-controller.js
@@ -6,33 +6,32 @@ function CardsController() {
 
   dataStore.getCharacters(ready)
 
+  this.onAdd = function () {
+    //this function will take the player that was clicked and add them to your team through the dataStore.
+
+  }
+
+  this.onRemove = function () {
+    //this will remove the character from your team
+  }
+
   function ready(data) {
-
     draw(data, [])
+  }
 
-    this.onAdd = function () {
-      //this function will take the player that was clicked and add them to your team through the dataStore.
+  function draw(marvelList, myList) {
+    //target is the id of the element where the list will be rendered
+    var marvelElem = document.getElementById('marvel-characters')
+    var myElem = document.getElementById('my-characters')
+    marvelElem.innerHTML = ''
+    myElem.innerHTML = ''
 
-    }
+    var marvelTemplate = ''
+    var myTemplate = ''
 
-    this.onRemove = function () {
-      //this will remove the character from your team
-    }
-
-
-    function draw(marvelList, myList) {
-      //target is the id of the element where the list will be rendered
-      var marvelElem = document.getElementById('marvel-characters')
-      var myElem = document.getElementById('my-characters')
-      marvelElem.innerHTML = ''
-      myElem.innerHTML = ''
-
-      var marvelTemplate = ''
-      var myTemplate = ''
-
-      for (var i in marvelList) {
-        var character = marvelList[i];
-        marvelTemplate += `
+    for (var i in marvelList) {
+      var character = marvelList[i];
+      marvelTemplate += `
           <div class="card">
             <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
             <h3>${character.name}</h3>
@@ -41,11 +40,11 @@ function CardsController() {
             </div>
           <div>
         `
-      }
+    }
 
-      for (var i in myList) {
-        var character = myList[i];
-        myTemplate += `
+    for (var i in myList) {
+      var character = myList[i];
+      myTemplate += `
           <div class="card">
             <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
             <h3>${character.name}</h3>
@@ -54,12 +53,10 @@ function CardsController() {
             </div>
           <div>
           `
-      }
-
-      marvelElem.innerHTML = marvelTemplate
-      myElem.innerHTML = myTemplate;
-
     }
+
+    marvelElem.innerHTML = marvelTemplate
+    myElem.innerHTML = myTemplate;
 
   }
 
