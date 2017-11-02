9c1e55679d6978f231162afa2124d391a6e24734
Jake Overall <joverall22@gmail.com>
2017-11-02 12:14:00 -0600

    Update cards-controller.js

--- cards-controller.js
+++ cards-controller.js
@@ -1,64 +1,36 @@
 function CardsController() {
+var marvelService = new globals.MarvelService()
 
-
-  var dataStore = new MarvelService()
-
-
-  dataStore.getCharacters(ready)
-
-  this.onAdd = function () {
-    //this function will take the player that was clicked and add them to your team through the dataStore.
-
+  this.add = function add(id){
+    console.log('characterId', id)
   }
 
-  this.onRemove = function () {
-    //this will remove the character from your team
-  }
+  marvelService.getCharacters(ready)
 
   function ready(data) {
-    draw(data, [])
-  }
 
-  function draw(marvelList, myList) {
-    //target is the id of the element where the list will be rendered
-    var marvelElem = document.getElementById('marvel-characters')
-    var myElem = document.getElementById('my-characters')
-    marvelElem.innerHTML = ''
-    myElem.innerHTML = ''
+    updateMarvel(data)
 
-    var marvelTemplate = ''
-    var myTemplate = ''
+    function updateMarvel(list) {
+      var elem = document.getElementById('marvel-characters')
+      elem.innerHTML = ''
+      var marvelTemplate = ''
+      for (var i in list) {
+        var character = list[i];
+        character.thumbnail.path = character.thumbnail.path.replace('http:', '')
+        marvelTemplate += `
+      <div class="card">
+        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
+        <h3>${character.name}</h3>
+        <div>
+          <button class="btn-success" id="${character.id}" onclick="app.controllers.marvelController.add('${character.id}')">Add to Team</button>
+        </div>
+      <div>
+      `
 
-    for (var i in marvelList) {
-      var character = marvelList[i];
-      marvelTemplate += `
-          <div class="card">
-            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
-            <h3>${character.name}</h3>
-            <div>
-              <button class="btn-success" id="${character.id}">Add to Team</button>
-            </div>
-          <div>
-        `
+        elem.innerHTML = marvelTemplate
+      }
+
     }
-
-    for (var i in myList) {
-      var character = myList[i];
-      myTemplate += `
-          <div class="card">
-            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
-            <h3>${character.name}</h3>
-            <div>
-              <button class="btn-danger" id="${character.id}">DESTROY FOREVER</button>
-            </div>
-          <div>
-          `
-    }
-
-    marvelElem.innerHTML = marvelTemplate
-    myElem.innerHTML = myTemplate;
-
-  }
-
 
 }
